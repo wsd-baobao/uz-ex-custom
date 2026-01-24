3e0d52ee3cbbf67edb76e599f1750bbfff743f62
wsd-baobao <a2245828322@hotmail.com>
2026-01-24 17:04:34 +0800

    不搞了

--- vod/js/jin_hohoj.js
+++ vod/js/jin_hohoj.js
@@ -1,271 +1,326 @@
 // ignore
 //@name:[禁] 好好j
-//@version:2
+// 网站主页，只有视频源扩展需要
 //@webSite:https://hohoj.tv
-//@remark:
-//@type:100
-//@instance:hohoj20240626
+// 版本号纯数字
+//@version:1
+// 备注，没有的话就不填
+//@remark:这是备注
+// 加密 id，没有的话就不填
+//@codeID:
+// 使用的环境变量，没有的话就不填
+//@env:
+// 是否是AV 1是  0否
 //@isAV:1
-//@order: E
-import { } from '../../core/uzVideo.js'
-import { } from '../../core/uzHome.js'
-import { } from '../../core/uz3lib.js'
-import { } from '../../core/uzUtils.js'
-// import { parse } from '../../core/cheerio.js'
+//是否弃用 1是  0否
+//@deprecated:0
+
 // ignore
 
-class hohojClass extends WebApiBase {
+
+
+// ignore
+// 不支持导入，这里只是本地开发用于代码提示
+// 如需添加通用依赖，请联系 https://t.me/uzVideoAppbot
+import {
+    FilterLabel,
+    FilterTitle,
+    VideoClass,
+    VideoSubclass,
+    VideoDetail,
+    RepVideoClassList,
+    RepVideoSubclassList,
+    RepVideoList,
+    RepVideoDetail,
+    RepVideoPlayUrl,
+    UZArgs,
+    UZSubclassVideoListArgs,
+} from '../core/uzVideo.js'
+
+import {
+    UZUtils,
+    ProData,
+    ReqResponseType,
+    ReqAddressType,
+    req,
+    getEnv,
+    setEnv,
+    goToVerify,
+    openWebToBindEnv,
+    toast,
+    kIsDesktop,
+    kIsAndroid,
+    kIsIOS,
+    kIsWindows,
+    kIsMacOS,
+    kIsTV,
+    kLocale,
+    kAppVersion,
+    formatBackData,
+} from '../core/uzUtils.js'
+
+import { cheerio, Crypto, Encrypt, JSONbig } from '../core/uz3lib.js'
+// ignore
+
+
+//MARK: 注意
+// 直接复制该文件进行扩展开发
+// 请保持以下 变量 及 函数 名称不变
+// 请勿删减，可以新增
+
+const appConfig = {
+    _webSite: 'https://hohoj.tv',
     /**
-     *
+     * 网站主页，uz 调用每个函数前都会进行赋值操作
+     * 如果不想被改变 请自定义一个变量
      */
-    constructor() {
-        super();
-        this.url = 'https://hohoj.tv'
-        this.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
-            'Host': "www.hohoj.tv"
-        }
-    }
+    get webSite() {
+        return this._webSite
+    },
+    set webSite(value) {
+        this._webSite = value
+    },
 
+    _uzTag: '',
     /**
-     * 异步获取分类列表的方法。
-     * @param {UZArgs} args
-     * @returns {Promise<RepVideoClassList>}
+     * 扩展标识，初次加载时，uz 会自动赋值，请勿修改
+     * 用于读取环境变量
      */
-    async getClassList(args) {
-        let webUrl = args.url
-        // 如果通过首页获取分类的话，可以将对象内部的首页更新
-        this.webSite = this.removeTrailingSlash(webUrl)
-        let backData = new RepVideoClassList()
+    get uzTag() {
+        return this._uzTag
+    },
+    set uzTag(value) {
+        this._uzTag = value
+    },
+    _headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
 
-        try {
-            let manualClassList = [
-                { type_name: "无新", type_id: "/search?type=uncensored&order=latest" },
-                { type_name: "无热", type_id: "/search?type=uncensored&order=popular" },
-                { type_name: "中热", type_id: "/search?type=chinese&order=popular" },
-                { type_name: "中新", type_id: "/search?type=chinese&order=latest" },
-                { type_name: "有新", type_id: "/search?type=censored&order=latest" },
-                { type_name: "欧新", type_id: "/search?type=europe&order=latest" },
-                { type_name: "多P", type_id: "/ctg?id=16&name=多P" }
-            ];
-
-            let list = []
-            for (let i = 0; i < manualClassList.length; i++) {
-                const item = manualClassList[i]
-                let videoClass = new VideoClass()
-                videoClass.type_id = item.type_id
-                videoClass.type_name = item.type_name
-                list.push(videoClass)
-            }
-            backData.data = list
-
-        } catch (error) {
-            backData.error = '获取分类失败～' + error.message
-        }
-        return JSON.stringify(backData)
-    }
-
-    /**
-     * 获取分类视频列表
-     * @param {UZArgs} args
-     * @returns {Promise<RepVideoList>}
-     */
-    async getVideoList(args) {
-        let listUrl = this.removeTrailingSlash(args.url) + '&p=' + args.page
-        let backData = new RepVideoList()
-        try {
-            let pro = await req(listUrl, { headers: this.headers })
-            backData.error = pro.error
-            let proData = pro.data
-            if (proData) {
-
-                if (!proData) {
-                    throw new Error('请求返回数据为空')
-                }
-
-                let document = parse(proData)
-
-                if (document == null) {
-                    throw new Error('解析HTML失败～' + document)
-                }
-
-                let allVideo = document.querySelectorAll('div.video-item')
-
-                if (allVideo.length === 0) {
-                    throw new Error('未找到视频元素' + allVideo)
-                }
-
-                let videos = []
-                for (let index = 0; index < allVideo.length; index++) {
-                    const element = allVideo[index]
-                    let vodUrl = element.querySelector('a')?.attributes['href'] ?? ''
-                    let vodPic = element.querySelector('img')?.attributes['src'] ?? ''
-                    let vodName = element.querySelector('div')?.text ?? ''
-                    let vodDiJiJi = element.querySelector('.me-2')?.text ?? ''
-
-                    let videoDet = {}
-                    videoDet.vod_id = vodUrl
-                    videoDet.vod_pic = vodPic
-                    videoDet.vod_name = vodName
-                    videoDet.vod_remarks = vodDiJiJi.trim()
-                    videos.push(videoDet)
-                }
-                backData.data = videos
-            }
-        } catch (error) {
-            backData.error = '获取列表失败～' + error.message
-        }
-        return JSON.stringify(backData)
-    }
-
-    /**
-     * 获取视频详情
-     * @param {UZArgs} args
-     * @returns {Promise<RepVideoDetail>}
-     */
-    async getVideoDetail(args) {
-        let backData = new RepVideoDetail()
-        try {
-            let webUrl = args.url
-            let pro = await req(webUrl, { headers: this.headers })
-            backData.error = pro.error
-            let proData = pro.data
-            if (proData) {
-                let document = parse(proData)
-                let vod_content = ''
-                let vod_pic = document.querySelector('.img-placeholder')?.attributes['src'] ?? ''
-                let vod_name = document.querySelector('.article-title')?.text ?? ''
-                let vod_year = ''
-                let vod_director = ''
-                let vod_actor = ''
-                let vod_area = ''
-                let vod_lang = ''
-                let vod_douban_score = ''
-                let type_name = document.querySelector('#latest-jav-carousel > div.glide__track > ul > li.glide__slide.glide__slide--active > div > div:nth-child(1) > a > div.video-item-title.mt-1')?.text ?? ''
-
-                let detModel = new VideoDetail()
-                detModel.vod_year = vod_year
-                detModel.type_name = type_name
-                detModel.vod_director = vod_director
-                detModel.vod_actor = vod_actor
-                detModel.vod_area = vod_area
-                detModel.vod_lang = vod_lang
-                detModel.vod_douban_score = vod_douban_score
-                detModel.vod_content = vod_content.trim()
-                detModel.vod_pic = vod_pic
-                detModel.vod_name = vod_name
-                detModel.vod_play_url = `$${webUrl}#`
-                detModel.vod_id = webUrl
-
-                backData.data = detModel
-            }
-        } catch (error) {
-            backData.error = '获取视频详情失败' + error.message
-        }
-
-        return JSON.stringify(backData)
-    }
-
-    /**
-     * 获取视频的播放地址
-     * @param {UZArgs} args
-     * @returns {Promise<RepVideoPlayUrl>}
-     */
-    async getVideoPlayUrl(args) {
-        let backData = new RepVideoPlayUrl()
-        let url = args.url
-        try {
-            let html = await req(url, { headers: this.headers })
-            backData.error = html.error
-            let document = parse(html.data)
-
-            let w = document.querySelector('body > div.container.mt-3 > div > div.col.player-col > iframe').getAttribute('src')
-            let dash = UZUtils.getHostFromURL(w)
-            let dashResp = (await req(w, { headers: this.headers })).data
-            let dashHtml = parse(dashResp)
-            let html2 = dashHtml.querySelectorAll('body script')[5].text
-            let token = html2.match(/var token = (.+);/)[1]
-            let m3u8 = html2.match(/var m3u8 = (.+);/)[1]
-
-            let play_url = dash + m3u8 + '?token=' + token
-
-            backData.data = play_url.replace(/'|"/gm, '')
-        } catch (error) {
-            backData.error = error.message
-        }
-        return JSON.stringify(backData)
-    }
-
-    /**
-     * 搜索视频
-     * @param {UZArgs} args
-     * @returns {Promise<RepVideoList>}
-     */
-    async searchVideo(args) {
-        let backData = new RepVideoList()
-        let url = this.removeTrailingSlash(this.url) + `/search?text=${args.searchWord}`
-
-        try {
-            let pro = await req(url, { headers: this.headers })
-            backData.error = pro.error
-            let proData = pro.data
-            if (proData) {
-                let document = parse(proData)
-                let allVideo = document.querySelector('body > div.search.container.mt-4 > div.video-list > div:nth-child(1)').querySelectorAll('div.video-item')
-                let videos = []
-                for (let index = 0; index < allVideo.length; index++) {
-                    const element = allVideo[index]
-                    let vodUrl = element.querySelector('a')?.attributes['href'] ?? ''
-                    let vodPic = element.querySelector('img')?.attributes['src'] ?? ''
-                    let vodName = element.querySelector('div')?.text ?? ''
-                    let vodDiJiJi = element.querySelector('.me-2')?.text ?? ''
-
-                    let videoDet = {}
-                    videoDet.vod_id = vodUrl
-                    videoDet.vod_pic = vodPic
-                    videoDet.vod_name = vodName
-                    videoDet.vod_remarks = vodDiJiJi.trim()
-                    videos.push(videoDet)
-                }
-                backData.data = videos
-            }
-        } catch (e) {
-            backData.error = e.message
-        }
-
-        return JSON.stringify(backData)
-    }
-
-    ignoreClassName = ['首页', '其他', '热门标签', '筛选']
-
-    combineUrl(url) {
-        if (url === undefined) {
-            return ''
-        }
-        if (url.indexOf(this.webSite) !== -1) {
-            return url
-        }
-        if (url.startsWith('/')) {
-            return this.webSite + url
-        }
-        return this.webSite + '/' + url
-    }
-
-    isIgnoreClassName(className) {
-        for (let index = 0; index < this.ignoreClassName.length; index++) {
-            const element = this.ignoreClassName[index]
-            if (className.indexOf(element) !== -1) {
-                return true
-            }
-        }
-        return false
-    }
-
-    removeTrailingSlash(str) {
-        if (str.endsWith('/')) {
-            return str.slice(0, -1)
-        }
-        return str
-    }
+    },
+    get headers() {
+        return this._headers
+    },
 }
-var hohoj20240626 = new hohojClass()
+
+/**
+ * 异步获取分类列表的方法。
+ * @param {UZArgs} args
+ * @returns {@Promise<JSON.stringify(new RepVideoClassList())>}
+ */
+async function getClassList(args) {
+    var backData = new RepVideoClassList()
+    try {
+        let manualClassList = [
+            { type_name: "无新", type_id: "/search?type=uncensored&order=latest" },
+            { type_name: "无热", type_id: "/search?type=uncensored&order=popular" },
+            { type_name: "中热", type_id: "/search?type=chinese&order=popular" },
+            { type_name: "中新", type_id: "/search?type=chinese&order=latest" },
+            { type_name: "有新", type_id: "/search?type=censored&order=latest" },
+            { type_name: "欧新", type_id: "/search?type=europe&order=latest" },
+            { type_name: "多P", type_id: "/ctg?id=16&name=多P" }
+        ];
+
+        let list = []
+        for (let i = 0; i < manualClassList.length; i++) {
+            const item = manualClassList[i]
+            let videoClass = new VideoClass()
+            videoClass.type_id = item.type_id
+            videoClass.type_name = item.type_name
+            list.push(videoClass)
+        }
+        backData.data = list
+
+    } catch (error) {
+        backData.error = error.toString()
+    }
+    return JSON.stringify(backData)
+}
+
+/**
+ * 获取二级分类列表筛选列表的方法。
+ * @param {UZArgs} args
+ * @returns {@Promise<JSON.stringify(new RepVideoSubclassList())>}
+ */
+async function getSubclassList(args) {
+    var backData = new RepVideoSubclassList()
+    try {
+    } catch (error) {
+        backData.error = error.toString()
+    }
+    return JSON.stringify(backData)
+}
+
+/**
+ * 获取分类视频列表
+ * @param {UZArgs} args
+ * @returns {@Promise<JSON.stringify(new RepVideoList())>}
+ */
+async function getVideoList(args) {
+    var backData = new RepVideoList()
+    let listUrl = appConfig.webSite + args.url
+    try {
+        let pro = await req(listUrl, { headers: appConfig.headers })
+        backData.error = pro.error
+        let proData = pro.data
+        if (proData) {
+            if (!proData) {
+                throw new Error('请求返回数据为空')
+            }
+            let document = parse(proData)
+            if (document == null) {
+                throw new Error('解析HTML失败～' + document)
+            }
+            let allVideo = document.querySelectorAll('div.video-item')
+            if (allVideo.length === 0) {
+                throw new Error('未找到视频元素' + allVideo)
+            }
+            let videos = []
+            for (let index = 0; index < allVideo.length; index++) {
+                const element = allVideo[index]
+                let vodUrl = element.querySelector('a')?.attributes['href'] ?? ''
+                let vodPic = element.querySelector('img')?.attributes['src'] ?? ''
+                let vodName = element.querySelector('div')?.text ?? ''
+                let vodDiJiJi = element.querySelector('.me-2')?.text ?? ''
+
+                let videoDet = {}
+                videoDet.vod_id = vodUrl
+                videoDet.vod_pic = vodPic
+                videoDet.vod_name = vodName
+                videoDet.vod_remarks = vodDiJiJi.trim()
+                videos.push(videoDet)
+            }
+            backData.data = videos
+        }
+    } catch (error) {
+        backData.error = error.toString()
+    }
+    return JSON.stringify(backData)
+}
+
+/**
+ * 获取二级分类视频列表 或 筛选视频列表
+ * @param {UZSubclassVideoListArgs} args
+ * @returns {@Promise<JSON.stringify(new RepVideoList())>}
+ */
+async function getSubclassVideoList(args) {
+    var backData = new RepVideoList()
+    try {
+    } catch (error) {
+        backData.error = error.toString()
+    }
+    return JSON.stringify(backData)
+}
+
+/**
+ * 获取视频详情
+ * @param {UZArgs} args
+ * @returns {@Promise<JSON.stringify(new RepVideoDetail())>}
+ */
+async function getVideoDetail(args) {
+    var backData = new RepVideoDetail()
+    try {
+        let webUrl = appConfig.webSite + args.url
+        let pro = await req(webUrl, { headers: appConfig.headers })
+        backData.error = pro.error
+        let proData = pro.data
+        if (proData) {
+            let document = parse(proData)
+            let vod_content = ''
+            //cdn图片，无法获取。
+            let vod_pic = document.querySelector('.img-placeholder').getAttribute('src') ?? ''
+            let vod_name = document.querySelector('.article-title')?.text ?? ''
+            let vod_year = ''
+            let vod_director = ''
+            let vod_actor = ''
+            let vod_area = ''
+            let vod_lang = ''
+            let vod_douban_score = ''
+            let type_name = document.querySelector('#latest-jav-carousel > div.glide__track > ul > li.glide__slide.glide__slide--active > div > div:nth-child(1) > a > div.video-item-title.mt-1')?.text ?? ''
+            let play_url = document.querySelector('body > div.container.mt-3 > div > div.col.player-col > iframe').getAttribute('src')
+
+            let detModel = new VideoDetail()
+            detModel.vod_year = vod_year
+            detModel.type_name = type_name
+            detModel.vod_director = vod_director
+            detModel.vod_actor = vod_actor
+            detModel.vod_area = vod_area
+            detModel.vod_lang = vod_lang
+            detModel.vod_douban_score = vod_douban_score
+            detModel.vod_content = vod_content.trim()
+            detModel.vod_pic = vod_pic
+            detModel.vod_name = vod_name
+            detModel.vod_play_url = `$${webUrl}#`
+            detModel.vod_id = args.url
+
+            backData.data = detModel
+        }
+    } catch (error) {
+        backData.error = error.toString()
+    }
+    return JSON.stringify(backData)
+}
+
+/**
+ * 获取视频的播放地址
+ * @param {UZArgs} args
+ * @returns {@Promise<JSON.stringify(new RepVideoPlayUrl())>}
+ */
+async function getVideoPlayUrl(args) {
+    var backData = new RepVideoPlayUrl()
+    let url = args.url
+    try {
+        let html = await req(url, { headers: appConfig.headers })
+        backData.error = html.error
+        let document = parse(html.data)
+        let w = document.querySelector('iframe.player').getAttribute('src')
+        let embed = await req(appConfig.webSite + w, { headers: appConfig.headers })
+        backData.error = embed.error
+        let embedData = embed.data
+        if (embedData) {
+            let document = parse(embedData)
+            let m3u8 = document.querySelector('body > script').text.match(/var videoSrc = (.+);/)[1]
+            backData.data = m3u8
+        }
+
+    } catch (error) {
+        backData.error = error.toString() + "||||" + w.toString()
+    }
+    return JSON.stringify(backData)
+}
+
+/**
+ * 搜索视频
+ * @param {UZArgs} args
+ * @returns {@Promise<JSON.stringify(new RepVideoList())>}
+ */
+async function searchVideo(args) {
+    var backData = new RepVideoList()
+    let url = appConfig.webSite + `/search?text=${args.searchWord}`
+    try {
+        let pro = await req(url, { headers: appConfig.headers })
+        backData.error = pro.error
+        let proData = pro.data
+        if (proData) {
+            let document = parse(proData)
+            let allVideo = document.querySelector('body > div.search.container.mt-4 > div.video-list > div:nth-child(1)').querySelectorAll('div.video-item')
+            let videos = []
+            for (let index = 0; index < allVideo.length; index++) {
+                const element = allVideo[index]
+                let vodUrl = element.querySelector('a')?.attributes['href'] ?? ''
+                let vodPic = element.querySelector('img')?.attributes['src'] ?? ''
+                let vodName = element.querySelector('div')?.text ?? ''
+                let vodDiJiJi = element.querySelector('.me-2')?.text ?? ''
+
+                let videoDet = {}
+                videoDet.vod_id = vodUrl
+                videoDet.vod_pic = vodPic
+                videoDet.vod_name = vodName
+                videoDet.vod_remarks = vodDiJiJi.trim()
+                videos.push(videoDet)
+            }
+            backData.data = videos
+        }
+    } catch (error) {
+        backData.error = error.toString()
+    }
+    return JSON.stringify(backData)
+}
