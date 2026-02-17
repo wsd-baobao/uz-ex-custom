afe0d62dcf2a3d7e2f23e0bdd52dcb2ef9a8c08f
wsd-baobao <a2245828322@hotmail.com>
2026-02-17 22:17:06 +0800

    添加phb

---
+++ vod/js/Jin_Pronhub.js
@@ -0,0 +1,341 @@
+// ignore
+//@name:[禁] pronhub
+// 网站主页，只有视频源扩展需要
+//@webSite:https://cn.pornhub.com
+// 版本号纯数字
+//@version:1
+// 备注，没有的话就不填
+//@remark:这是备注
+// 加密 id，没有的话就不填
+//@codeID:
+// 使用的环境变量，没有的话就不填
+//@env:
+// 是否是AV 1是  0否
+//@isAV:1
+//是否弃用 1是  0否
+//@deprecated:0
+
+// ignore
+
+
+
+// ignore
+// 不支持导入，这里只是本地开发用于代码提示
+// 如需添加通用依赖，请联系 https://t.me/uzVideoAppbot
+import {
+    FilterLabel,
+    FilterTitle,
+    VideoClass,
+    VideoSubclass,
+    VideoDetail,
+    RepVideoClassList,
+    RepVideoSubclassList,
+    RepVideoList,
+    RepVideoDetail,
+    RepVideoPlayUrl,
+    UZArgs,
+    UZSubclassVideoListArgs,
+} from '../core/uzVideo.js'
+
+import {
+    UZUtils,
+    ProData,
+    ReqResponseType,
+    ReqAddressType,
+    req,
+    getEnv,
+    setEnv,
+    goToVerify,
+    openWebToBindEnv,
+    toast,
+    kIsDesktop,
+    kIsAndroid,
+    kIsIOS,
+    kIsWindows,
+    kIsMacOS,
+    kIsTV,
+    kLocale,
+    kAppVersion,
+    formatBackData,
+} from '../core/uzUtils.js'
+
+import { cheerio, Crypto, Encrypt, JSONbig } from '../core/uz3lib.js'
+// ignore
+
+
+//MARK: 注意
+// 直接复制该文件进行扩展开发
+// 请保持以下 变量 及 函数 名称不变
+// 请勿删减，可以新增
+
+const appConfig = {
+    _webSite: 'https://cn.pornhub.com',
+    /**
+     * 网站主页，uz 调用每个函数前都会进行赋值操作
+     * 如果不想被改变 请自定义一个变量
+     */
+    get webSite() {
+        return this._webSite
+    },
+    set webSite(value) {
+        this._webSite = value
+    },
+
+    _uzTag: '',
+    /**
+     * 扩展标识，初次加载时，uz 会自动赋值，请勿修改
+     * 用于读取环境变量
+     */
+    get uzTag() {
+        return this._uzTag
+    },
+    set uzTag(value) {
+        this._uzTag = value
+    },
+    _headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+        'Referer': 'https://cn.pornhub.com',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'Connection': 'keep-alive',
+        'host': 'pornhub.com',
+    },
+    get headers() {
+        return this._headers
+    },
+}
+
+/**
+ * 异步获取分类列表的方法。
+ * @param {UZArgs} args
+ * @returns {@Promise<JSON.stringify(new RepVideoClassList())>}
+ */
+async function getClassList(args) {
+    let listUrl = appConfig.webSite + '/categories'
+
+    var backData = new RepVideoClassList()
+    try {
+        let pro = await req(listUrl, { headers: appConfig.headers })
+        backData.error = pro.error
+        let proData = pro.data
+        if (proData) {
+            let document = parse(proData)
+            let allClass = document.querySelectorAll('#categoriesListingWrapper')[1].querySelectorAll('a.js-flipbookOn.js-videoPreview.webm-videoPreview')
+            let list = []
+            for (let i = 0; i < allClass.length; i++) {
+                const item = allClass[i]
+                let videoClass = new VideoClass()
+                videoClass.type_id = item.getAttribute('href')
+                videoClass.type_name = item.getAttribute('alt')
+                list.push(videoClass)
+            }
+            backData.data = list
+        }
+
+
+    } catch (error) {
+        backData.error = error.toString()
+    }
+    return JSON.stringify(backData)
+}
+
+/**
+ * 获取二级分类列表筛选列表的方法。
+ * @param {UZArgs} args
+ * @returns {@Promise<JSON.stringify(new RepVideoSubclassList())>}
+ */
+async function getSubclassList(args) {
+    var backData = new RepVideoSubclassList()
+    try {
+    } catch (error) {
+        backData.error = error.toString()
+    }
+    return JSON.stringify(backData)
+}
+
+/**
+ * 获取分类视频列表
+ * @param {UZArgs} args
+ * @returns {@Promise<JSON.stringify(new RepVideoList())>}
+ */
+async function getVideoList(args) {
+    var backData = new RepVideoList()
+    let listUrl = appConfig.webSite + args.url
+    listUrl = listUrl.includes('/categories') ||
+        listUrl.includes('/vr') ||
+        listUrl.includes('/hd') ||
+        listUrl.includes('/sfw') ||
+        listUrl.includes('/popularwithwomen') ||
+        listUrl.includes('/interactive') ||
+        listUrl.includes('/transgender')
+
+        ? listUrl + '?page=' + args.page : listUrl + '&page=' + args.page
+    try {
+        let pro = await req(listUrl, { headers: appConfig.headers })
+        backData.error = pro.error
+        let proData = pro.data
+        if (proData) {
+            if (!proData) {
+                throw new Error('请求返回数据为空')
+            }
+            let document = parse(proData)
+            if (document == null) {
+                throw new Error('解析HTML失败～' + document)
+            }
+            let allVideo = document.querySelectorAll("li.pcVideoListItem.js-pop.videoblock.videoBox.withKebabMenu a.fade.videoPreviewBg.linkVideoThumb.js-linkVideoThumb.img.fadeUp")
+
+            if (allVideo.length === 0) {
+                throw new Error('未找到视频元素' + allVideo + ' ' + listUrl)
+            }
+            let videos = []
+            for (let index = 0; index < allVideo.length; index++) {
+                const element = allVideo[index]
+                let vodUrl = element?.attributes['href'] ?? ''
+                let vodPic = element.querySelector('img')?.attributes['src'] ?? ''
+                let vodName = element.querySelector('img')?.attributes['alt'] ?? ''
+                let vodDiJiJi = element.querySelector('.duration')?.text ?? ''
+
+                let videoDet = {}
+                videoDet.vod_id = vodUrl
+                // videoDet.vod_pic = vodPic
+                videoDet.vod_name = vodName
+                videoDet.vod_remarks = vodDiJiJi.trim()
+                videos.push(videoDet)
+            }
+            backData.data = videos
+        }
+    } catch (error) {
+        backData.error = error.toString()
+    }
+    return JSON.stringify(backData)
+}
+
+/**
+ * 获取二级分类视频列表 或 筛选视频列表
+ * @param {UZSubclassVideoListArgs} args
+ * @returns {@Promise<JSON.stringify(new RepVideoList())>}
+ */
+async function getSubclassVideoList(args) {
+    var backData = new RepVideoList()
+    try {
+    } catch (error) {
+        backData.error = error.toString()
+    }
+    return JSON.stringify(backData)
+}
+
+/**
+ * 获取视频详情
+ * @param {UZArgs} args
+ * @returns {@Promise<JSON.stringify(new RepVideoDetail())>}
+ */
+async function getVideoDetail(args) {
+    var backData = new RepVideoDetail()
+    try {
+        let webUrl = appConfig.webSite + args.url
+        let pro = await req(webUrl, { headers: appConfig.headers })
+        backData.error = pro.error
+        let proData = pro.data
+        if (proData) {
+            let document = parse(proData)
+            let vod_content = ''
+            //cdn图片，无法获取。
+            let vod_pic = ''
+            let vod_name = document.querySelector('.article-title')?.text ?? ''
+            let vod_year = ''
+            let vod_director = ''
+            let vod_actor = ''
+            let vod_area = ''
+            let vod_lang = ''
+            let vod_douban_score = ''
+            let type_name = document.querySelector('#latest-jav-carousel > div.glide__track > ul > li.glide__slide.glide__slide--active > div > div:nth-child(1) > a > div.video-item-title.mt-1')?.text ?? ''
+            let play_url = document.querySelector('meta[property="og:video:url"]').content
+
+            let detModel = new VideoDetail()
+            detModel.vod_year = vod_year
+            detModel.type_name = type_name
+            detModel.vod_director = vod_director
+            detModel.vod_actor = vod_actor
+            detModel.vod_area = vod_area
+            detModel.vod_lang = vod_lang
+            detModel.vod_douban_score = vod_douban_score
+            detModel.vod_content = vod_content.trim()
+            detModel.vod_pic = vod_pic
+            detModel.vod_name = vod_name
+            detModel.vod_play_url = play_url
+            //`$${webUrl}#`
+            detModel.vod_id = args.url
+
+            backData.data = detModel
+        }
+    } catch (error) {
+        backData.error = error.toString()
+    }
+    return JSON.stringify(backData)
+}
+
+/**
+ * 获取视频的播放地址
+ * @param {UZArgs} args
+ * @returns {@Promise<JSON.stringify(new RepVideoPlayUrl())>}
+ */
+async function getVideoPlayUrl(args) {
+    var backData = new RepVideoPlayUrl()
+    let url = args.url
+    try {
+        let html = await req(url, { headers: appConfig.headers })
+        backData.error = html.error
+        let document = parse(html.data)
+        let w = document.querySelector('iframe.player').getAttribute('src')
+        let embed = await req(appConfig.webSite + w, { headers: appConfig.headers })
+        backData.error = embed.error
+        let embedData = embed.data
+        if (embedData) {
+            let document = parse(embedData)
+            let m3u8 = document.querySelector('body > script').text.match(/var videoSrc = (.+);/)[1]
+            backData.data = m3u8
+        }
+
+    } catch (error) {
+        backData.error = error.toString() + "||||" + w.toString()
+    }
+    return JSON.stringify(backData)
+}
+
+/**
+ * 搜索视频
+ * @param {UZArgs} args
+ * @returns {@Promise<JSON.stringify(new RepVideoList())>}
+ */
+async function searchVideo(args) {
+    var backData = new RepVideoList()
+    let url = appConfig.webSite + `/search?text=${args.searchWord}`
+    try {
+        let pro = await req(url, { headers: appConfig.headers })
+        backData.error = pro.error
+        let proData = pro.data
+        if (proData) {
+            let document = parse(proData)
+            let allVideo = document.querySelector('body > div.search.container.mt-4 > div.video-list > div:nth-child(1)').querySelectorAll('div.video-item')
+            let videos = []
+            for (let index = 0; index < allVideo.length; index++) {
+                const element = allVideo[index]
+                let vodUrl = element.querySelector('a')?.attributes['href'] ?? ''
+                let vodPic = element.querySelector('img')?.attributes['src'] ?? ''
+                let vodName = element.querySelector('div')?.text ?? ''
+                let vodDiJiJi = element.querySelector('.me-2')?.text ?? ''
+
+                let videoDet = {}
+                videoDet.vod_id = vodUrl
+                videoDet.vod_pic = vodPic
+                videoDet.vod_name = vodName
+                videoDet.vod_remarks = vodDiJiJi.trim()
+                videos.push(videoDet)
+            }
+            backData.data = videos
+        }
+    } catch (error) {
+        backData.error = error.toString()
+    }
+    return JSON.stringify(backData)
+}
